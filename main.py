f5e86756a89a83521cf2a93311dcd2ef95c01318
DoNjOnIn <kaijaroslav@gmail.com>
2021-10-19 21:59:18 +0300

    Unit_test

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import math
 
-def f(a,b,c):
+def func(a,b,c):
     return math.sin(a*b*c)/(a*a+b*b+c*c)
 
 def main():
     s = float(input('s='))
     t = float(input('t='))
-    p=(f(1,t*t,s)+f(t,s*s,1))/(1+f(1,t*s,1)**2)
+    p=(func(1,t*t,s)+func(t,s*s,1))/(1+func(1,t*s,1)**2)
     print(p)
 
 if __name__ == '__main__':
