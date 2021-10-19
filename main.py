4a8076cbad5b5325dfde3613db9cf1dcc7e2390a
DoNjOnIn <kaijaroslav@gmail.com>
2021-10-19 22:21:07 +0300

    Unit_test

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def func(a,b,c):
     return math.sin(a*b*c)/(a*a+b*b+c*c)
 
 def main():
-    s = float(input('s='))
-    t = float(input('t='))
+    s = float(input('s= '))
+    t = float(input('t= '))
     p=(func(1,t*t,s)+func(t,s*s,1))/(1+func(1,t*s,1)**2)
     print(p)
 
